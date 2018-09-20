f79f112fad607c76ff44cbf9f9d9ebd8afd7670e
Alain Benbassat <alain@businessandcode.eu>
2018-09-20 10:07:20 +0200

    initial version

---
+++ CRM/Timelab/Event.php
@@ -0,0 +1,34 @@
+<?php
+
+class CRM_Timelab_Event {
+  public function getEventDetails($id) {
+    $params = [
+      'sequential' => 1,
+      'id' => $id,
+    ];
+
+    $e = civicrm_api3('Event', 'getsingle', $params);
+    return $e;
+  }
+
+  public function getEventList($fromDate, $toDate) {
+    $sql = "
+      SELECT
+        id
+        , title
+        , start_date
+        , end_date
+      FROM
+        civicrm_event
+      WHERE
+        start_date between %1 and %2
+    ";
+    $sqlParams = [
+      1 => [$fromDate . '00:00:00', 'String'],
+      2 => [$toDate . ' 23:59:59', 'String'],
+    ];
+
+    $dao = CRM_Core_DAO::executeQuery($sql, $sqlParams);
+    return $dao->fetchAll();
+  }
+}
---
+++ api/v3/Timelab/Geteventlist.php
@@ -0,0 +1,33 @@
+<?php
+
+function _civicrm_api3_timelab_Geteventlist_spec(&$spec) {
+}
+
+function civicrm_api3_timelab_Geteventlist($params) {
+  try {
+    // check if from date was specified
+    if (array_key_exists('from_date', $params)) {
+      $fromDate = $params['from_date'];
+    }
+    else {
+      $fromDate = date('Y-m-d');
+    }
+
+    // check if to date was specified
+    if (array_key_exists('to_date', $params)) {
+      $toDate = $params['to_date'];
+    }
+    else {
+      $toDate = '2999-21-31';
+    }
+
+    $eventHelper = new CRM_Timelab_Event();
+    $events = $eventHelper->getEventList($fromDate, $toDate);
+
+    return civicrm_api3_create_success($events, $params, 'Timelab', 'getEventList');
+  }
+  catch (Exception $e) {
+    throw new API_Exception($e->getMessage(), $e->getCode());
+  }
+}
+
