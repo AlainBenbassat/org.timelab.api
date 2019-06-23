09ae2d7063409597891996c5978cd0e7592ed9e1
Thomas Weyn <thomas@weynwebworks.com>
2019-06-23 19:32:03 +0200

    Fetch project events too

--- api/v3/Timelab/Getproject.php
+++ api/v3/Timelab/Getproject.php
@@ -81,6 +81,52 @@ function civicrm_api3_timelab_Getproject($params) {
         }
         $project[0]['people'] = $people;
 
+        // get events
+        $sql = "
+          select
+            e.id
+            , e.title
+            , e.start_date
+            , e.end_date
+            , e.summary
+            , e.event_type_id
+            , e.is_monetary
+            , ov.label as event_type
+            , concat(%2, 'sites/all/files/civicrm/custom/', f.uri) as image
+            , i.stroom_43 as stroom
+          from
+            civicrm_event e
+          inner join
+            civicrm_option_value ov on ov.value = e.event_type_id
+          inner join 
+            civicrm_option_group og on ov.option_group_id = og.id and og.name = 'event_type'
+          left outer join 
+            civicrm_value_img_9 i on i.entity_id = e.id
+          left outer join 
+            civicrm_file f on i.featured_image_25 = f.id
+          where 
+            e.is_active = 1
+          and
+            e.is_public = 1
+          and 
+            i.project_45 = %1";
+        $sqlParams = [
+            1 => [$project[0]['id'], 'Integer'],
+            2 => [CRM_Utils_System::baseURL(), 'String']
+        ];
+
+        $events = [];
+        $dao = CRM_Core_DAO::executeQuery($sql, $sqlParams);
+        while ($dao->fetch()) {
+            $event = $dao->toArray();
+            $event['event_type'] = [
+                'id' => $dao->event_type_id,
+                'name' => $dao->event_type,
+            ];
+            $events[] = $event;
+        }
+        $project[0]['events'] = $events;
+
         return civicrm_api3_create_success($project, $params, 'Timelab', 'Getproject');
     }
     catch (Exception $e) {
