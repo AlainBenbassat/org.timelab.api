b41f43d4a295a4aec8ea51153f85cd27ba19e284
Thomas Weyn <thomas@weynwebworks.com>
2019-08-13 02:26:07 +0200

    Fixed fetching of people

--- api/v3/Timelab/Getpeople.php
+++ api/v3/Timelab/Getpeople.php
@@ -9,18 +9,23 @@ function civicrm_api3_timelab_Getpeople($params) {
         $extrajoins  = '';
         $extrafields = '';
         $filterProjects = "";
-        if (array_key_exists('project', $params)) {
-            $filterProjects = 'and r.contact_id_b = '.intval($params['project']);
-            $extrajoins .= ' inner join civicrm_contact as cb on r.contact_id_b = cb.id';
+        if (array_key_exists('project', $params) || !array_key_exists('relationship_type', $params)) {
+            if (array_key_exists('project', $params)) {
+              $filterProjects .= 'and r.contact_id_b = ' . intval($params['project']);
+            }
+            if($params['project'] == 2402) { // timelab
+                $extrafields .= ', GROUP_CONCAT(DISTINCT(e.email)) as email, GROUP_CONCAT(DISTINCT(tel.phone)) as phone';
+                $extrajoins .= ' left join civicrm_phone as tel on tel.contact_id = c.id left join civicrm_email as e on e.contact_id = c.id';
+            }
+            else {
+                $filterProjects .= 'and cb.is_deleted = 0 and cb.contact_type = \'Organization\' ' .
+                                  'and cb.contact_sub_type = \'Project\' ';
+                $extrajoins .= '';
+            }
         }
         $filterRelationshipType = "";
         if (array_key_exists('relationship_type', $params)) {
             $filterRelationshipType = 'and r.relationship_type_id = '.intval($params['relationship_type']);
-        }
-
-        if($params['project'] == 2402) { // timelab
-            $extrafields .= ', GROUP_CONCAT(DISTINCT(e.email)) as email, GROUP_CONCAT(DISTINCT(tel.phone)) as phone';
-            $extrajoins .= ' left join civicrm_phone as tel on tel.contact_id = c.id left join civicrm_email as e on e.contact_id = c.id';
         }
 
         $sql = "
@@ -36,11 +41,12 @@ function civicrm_api3_timelab_Getpeople($params) {
             civicrm_value_public_5 as p on c.id = p.entity_id
           inner join
             civicrm_relationship as r on r.contact_id_a = c.id
+          inner join
+            civicrm_contact as cb on r.contact_id_b = cb.id
           $extrajoins
           where
             c.is_deleted = 0
-            and (r.end_date IS NULL or r.end_date > NOW()) " .
-          ($filterProjects ? "" : ("and cb.is_deleted = 0 and cb.contact_type = 'Organization' and cb.contact_sub_type = 'Project'")) . "
+            and (r.end_date IS NULL or r.end_date > NOW()) 
             $filterProjects
             $filterRelationshipType
           group by
