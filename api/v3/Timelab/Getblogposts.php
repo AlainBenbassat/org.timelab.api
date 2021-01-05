96f21982d251f011ed8bb4ee140fd1f6f751568d
Thomas Weyn <thomas@weynwebworks.com>
2021-01-05 10:55:39 +0100

    Fixed getblogposts SQL query

--- api/v3/Timelab/Getblogposts.php
+++ api/v3/Timelab/Getblogposts.php
@@ -6,15 +6,17 @@ function _civicrm_api3_timelab_Getblogposts_spec(&$spec) {
 function civicrm_api3_timelab_Getblogposts($params) {
   try {
     $sql = "
-      select distinct
+      select
         e.id,
         e.title,
-        e.start_date as date,
-        concat(%3, 'sites/all/files/civicrm/custom/', f.uri) as image,
+        e.start_date as `date`,
+        concat(%1, 'sites/all/files/civicrm/custom/', f.uri) as image,
         f.id as image_file_id
       from
         civicrm_event e
-      inner join
+      left outer join
+        civicrm_value_img_9 i on i.entity_id = e.id
+      left outer join
         civicrm_file f on i.featured_image_25 = f.id
       where
         e.is_active = 1 AND
@@ -22,7 +24,8 @@ function civicrm_api3_timelab_Getblogposts($params) {
       order by
         e.start_date DESC";
 
-    $events = CRM_Core_DAO::executeQuery($sql)->fetchAll();
+    $events = CRM_Core_DAO::executeQuery($sql, [1 => [CRM_Utils_System::baseURL(), 'String']]);
+    $events = $events->fetchAll();
 
     return civicrm_api3_create_success($events, $params, 'Timelab', 'Getblogposts');
   }
